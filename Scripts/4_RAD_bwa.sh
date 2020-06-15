b0cd259d06e13269d2969a1cf7a82184d98de439
jlastoll <52936002+jlastoll@users.noreply.github.com>
2020-06-15 16:05:37 -0400

    Update 4_RAD_bwa.sh
    added code to generate combined mapping stats

--- Scripts/4_RAD_bwa.sh
+++ Scripts/4_RAD_bwa.sh
@@ -42,3 +42,24 @@ samtools flagstat "$sample".bam \
 2> ../4_mapped_bam_files/sort_flagstat/"$sample"_flgstbam.stderr
 
 done
+
+wait
+
+#this is just combining all the bam stats files for each sample into one text file that is tab delimited so one sample per row so can then easily manipulate to summarize mapping stats etc
+
+for filename in *_flagstat.txt; do
+    cat "$filename"|tr '\n' '\t'
+    echo "$filename"
+done > ./All_RAD_sort_combined.flagstat_tabbed.txt
+
+wait
+
+#few lines to generate a cleaned up summary file of mapping stats for all samples to compare pre and post filtering
+
+awk '{print NF}' All_RAD_sort_combined.flagstat_tabbed.txt #88 columns
+awk '{print $88 "\t" $1 "\t" $23 "\t" $27 "\t"$30 "\t"$44 "\t" $49 "\t" $52 "\t"$60 "\t" $64 "\t" $67 "\t" $77 }' All_RAD_sort_filt_combined.flagstat_tabbed.txt >All_Rapture_flagstat_reformat.txt
+sed -i 's/(//g' All_RAD_sort_combined_flagstat_reformat.txt #-i changes it in the original file; could test sending to another outfile first
+sed -i 's/%//g' All_RAD_sort_combined_flagstat_reformat.txt
+cat ../short_flagstat_headers All_RAD_sort_combined_flagstat_reformat.txt >All_head_flagstat_reformat.txt
+
+done
