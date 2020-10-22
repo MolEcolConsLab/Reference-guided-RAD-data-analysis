0ba1faaef2deb79a0e1b44a62233200286a25851
jlastoll <52936002+jlastoll@users.noreply.github.com>
2020-10-22 16:58:17 -0400

    Update 5_RAD_remove_dups.sh

--- Scripts/5_RAD_remove_dups.sh
+++ Scripts/5_RAD_remove_dups.sh
@@ -6,17 +6,16 @@
 #BSUB -R rusage[mem=1600]
 #BSUB -n 6
 #BSUB -R span[hosts=1]
-#BSUB -e RADe_rmdups.err
+#BSUB -e RAD_rmdups.err
 #BSUB -oo RAD_rmdups.log
 
 
 module load samtools/1.9
-mkdir ../5_filtered_bam_files
 for file in *_sort.bam
 
 do
 
-sample=`echo $file | cut -f1,2,3,4 -d "_" `
+sample=`echo $file | cut -f1,2,3,4,5 -d "_" ` #change to match number of fields in your sample name
 
 echo $sample
 
@@ -24,7 +23,6 @@ samtools rmdup "$sample"_sort.bam ../5_filtered_bam_files/"$sample"_sortfltr.bam
 
 done
 
-wait
 
 cd ../5_filtered_bam_files/
 mkdir ./sort_filt_flagstat
@@ -35,13 +33,12 @@ do
 sample=`echo $file | cut -f1 -d "." `
 
 echo $sample
-samtools flagstat "$sample"_sortfltr.bam \
-> ./sort_filt_flagstat/"$sample"_sortfltr_flagstat.txt \
-2> ./sort_filt_flagstat/"$sample"_sortfltr_flgstbam.stderr
+samtools flagstat "$sample".bam \
+> ./sort_filt_flagstat/"$sample"_flagstat.txt \
+2> ./sort_filt_flagstat/"$sample"_flgstbam.stderr
 
 done
 
-wait
 
 cd ./sort_filt_flagstat/
 #this is just combining all the bam stats files for each sample into one text file that is tab delimited so one sample per row so can then easily manipulate to summarize mapping stats etc
@@ -51,13 +48,10 @@ for filename in *_sortfltr_flagstat.txt; do
     echo "$filename"
 done > ./All_RAD_sort_filt_combined.flagstat_tabbed.txt
 
-wait
 #few lines to generate a cleaned up summary file of mapping stats post filtering
 
 awk '{print NF}' All_RAD_sort_filt_combined.flagstat_tabbed.txt #88 columns
-awk '{print $88 "\t" $1 "\t" $23 "\t" $27 "\t"$30 "\t"$44 "\t" $49 "\t" $52 "\t"$60 "\t" $64 "\t" $67 "\t" $77 }' All_RAD_sort_filt_combined.flagstat_tabbed.txt >All_Rapture_flagstat_reformat.txt
+awk '{print $88 "\t" $1 "\t" $23 "\t" $27 "\t"$30 "\t"$44 "\t" $49 "\t" $52 "\t"$60 "\t" $64 "\t" $67 "\t" $77 }' All_RAD_sort_filt_combined.flagstat_tabbed.txt > All_RAD_sort_filt_combined_flagstat_reformat.txt
 sed -i 's/(//g' All_RAD_sort_filt_combined_flagstat_reformat.txt #-i changes it in the original file; could test sending to another outfile first
 sed -i 's/%//g' All_RAD_sort_filt_combined_flagstat_reformat.txt
-cat ../short_flagstat_headers All_RAD_sort_filt_combined_flagstat_reformat.txt >All_head_filt_flagstat_reformat.txt
-
-done
+cat ../../scripts_and_keyfiles/short_flagstat_headers All_RAD_sort_filt_combined_flagstat_reformat.txt >All_head_filt_flagstat_reformat.txt
